f3bb359138a9d9728d6ccafea0917aa6ec17e5da
Song Tuan <75135018+phamtuan1152001@users.noreply.github.com>
2021-05-02 09:28:04 +0700

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,153 @@
+package E_learning_Platform;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.awt.event.ActionEvent;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField usertext;
+	private JPasswordField passtext;
+	protected JLabel txtuser;
+	protected JLabel txtpassword;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setForeground(Color.WHITE);
+		panel_1.setBackground(Color.ORANGE);
+		panel_1.setBounds(10, 11, 131, 229);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("LOGIN:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(10, 91, 111, 37);
+		panel_1.add(lblNewLabel);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.GRAY);
+		panel_2.setBounds(151, 11, 263, 229);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel lbluser = new JLabel("Username:");
+		lbluser.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lbluser.setBounds(10, 74, 69, 31);
+		panel_2.add(lbluser);
+		
+		JLabel lblpass = new JLabel("Password:");
+		lblpass.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblpass.setBounds(10, 116, 69, 14);
+		panel_2.add(lblpass);
+		
+		usertext = new JTextField();
+		usertext.setBounds(89, 80, 164, 20);
+		panel_2.add(usertext);
+		usertext.setColumns(10);
+		
+		passtext = new JPasswordField();
+		passtext.setBounds(89, 114, 164, 20);
+		panel_2.add(passtext);
+		
+		JButton btndangnhap = new JButton("Login");
+		btndangnhap.addActionListener(new ActionListener() {
+			@SuppressWarnings("deprecation")
+			public void actionPerformed(ActionEvent e) {
+				if (usertext.getText().equals("")) {
+					JOptionPane.showMessageDialog(rootPane, "Please enter your username!");
+				}else if(passtext.getText().equals("")) {
+					JOptionPane.showMessageDialog(rootPane, "Please enter your password!");
+				}else {
+					try {
+						String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+						String url = "jdbc:sqlserver://DESKTOP-JTBVOBK:1433;databaseName=LOGIN";
+						Class.forName(driver);
+						Connection con = DriverManager.getConnection(url);
+						String sql = "SELECT*FROM ACCOUNT\n" + 
+						"WHERE USERNAME=? AND PASS=?";
+						PreparedStatement ps = con.prepareStatement(sql);
+						ps.setString(1, usertext.getText());
+						ps.setString(2, passtext.getText());
+						
+						ResultSet rs = ps.executeQuery();
+					if (rs.next()) {
+						JOptionPane.showMessageDialog(rootPane, "Congratulate on your accessing successfully!");
+					}
+					else {
+						JOptionPane.showMessageDialog(rootPane, "Sorry for your accessing fail!");	
+					}				
+				       }
+				catch(Exception e1) {
+					System.out.print(e1);
+				}
+			 }
+			}
+			});
+		btndangnhap.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btndangnhap.setBounds(10, 159, 89, 23);
+		panel_2.add(btndangnhap);
+		
+		JButton btnreg = new JButton("Register");
+		btnreg.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RegisterAccount ra = new RegisterAccount();
+				ra.setVisible(true);
+			}
+		});
+		btnreg.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnreg.setBounds(164, 159, 89, 23);
+		panel_2.add(btnreg);
+	}
+}
+
+
